dfe63a89169daac6dfcac8918e323957187b41ab
Alberto Poblacion <alberto.poblacion@travelportdigital.com>
2017-07-19 21:04:32 +0100

    * Added menu. * TODO Change menu icon. * TODO start with UI

--- app/src/main/java/com/android/pobla/popularmovies/main/MainActivity.java
+++ app/src/main/java/com/android/pobla/popularmovies/main/MainActivity.java
@@ -2,6 +2,8 @@ package com.android.pobla.popularmovies.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -30,5 +32,19 @@ public class MainActivity extends AppCompatActivity implements MainView {
   }
 
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
 
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (R.id.action_refresh == item.getItemId()) {
+      presenter.refreshContent();
+      return true;
+    } else {
+      return super.onOptionsItemSelected(item);
+    }
+  }
 }
